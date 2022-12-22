f902d2423ce134dbc1db03fb5da39a7676a56485
saadkhan3977 <saadkhan3977@gmail.com>
2022-12-22 16:26:47 +0500

    saad commitid this code payment with paypals

--- config/paypal.php
+++ config/paypal.php
@@ -1,7 +1,7 @@
 <?php 
 return [ 
-    'client_id' => 'AYlp5qIwDbFMgWP1zlYES0h3mvxYkQFKWAht26vkJd6BeK1kiN39HxcYevn03HA5H3dWdQC4ZyYGLk6j',
-	'secret' => 'EJ_hmKgf9UuS38YCEyCN10NF2FLPVz8DIB1sjMBShuMG_cHH4yaZpTvA0LpUFFzBi4-hHzhKUZS7KCQw',
+    // 'client_id' => 'AYlp5qIwDbFMgWP1zlYES0h3mvxYkQFKWAht26vkJd6BeK1kiN39HxcYevn03HA5H3dWdQC4ZyYGLk6j',
+	// 'secret' => 'EJ_hmKgf9UuS38YCEyCN10NF2FLPVz8DIB1sjMBShuMG_cHH4yaZpTvA0LpUFFzBi4-hHzhKUZS7KCQw',
     'settings' => array(
         'mode' => 'sandbox',
         'http.ConnectionTimeOut' => 1000,
